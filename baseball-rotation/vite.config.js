81668a060aea141fac1d710bac573a92f32861f0
wrightty <wrightty@hdsb.ca>
2026-05-28 18:27:19 +0000

    changed github repo name

--- baseball-rotation/vite.config.js
+++ baseball-rotation/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/HDBA-11U-Fielding-Rotation-Validator/'
+  base: '/Baseball-Rotation-Planner/'
 })
